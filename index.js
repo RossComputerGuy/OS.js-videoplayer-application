ea90bd1e3b73dffac7309e26368f67b2a82a6c9d
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-30 09:28:35 -0800

    added locales and keywords

--- index.js
+++ index.js
@@ -10,34 +10,29 @@ import {
 	Box,BoxContainer,Button,Icon,Menubar,MenubarItem,Video
 } from "@osjs/gui";
 
-const createAboutDialog = core => {
+const createAboutDialog = (core,_) => {
 	core.make("osjs/dialogs").create({
 		buttons: ["ok"],
-		window: { title: "About", dimension: { width: 500, height: 200 } }
+		window: { title: _("ABOUT_TITLE"), dimension: { width: 500, height: 200 } }
 	},dialog => null,(btn,value) => {
 		return value;
 	}).render(($content,dialogWindow,dialog) => {
-		dialog.app = app({
-			from: "", to: "", body: ""
-		},{
-			setInput: ({ name, value }) => () => ({[name]: value}),
-			getState: () => state => state
-		},
+		dialog.app = app({},{},
 		(state,actions) => dialog.createView([
 			h(Box,{ grow: 1, padding: false },[
-				h(BoxContainer,{},"Video Player - Version 1.0.0"),
-				h(BoxContainer,{},"Developed By Spaceboy Ross (https://youtube.com/c/SpaceboyRoss)"),
-				h(BoxContainer,{},"Licenced under Apache 2.0"),
-				h(BoxContainer,{},"A simple video player for OS.js V3 based on the Preview application for OS.js V3.")
+				h(BoxContainer,{},_("ABOUT_VERSION")),
+				h(BoxContainer,{},_("ABOUT_DEV")),
+				h(BoxContainer,{},_("ABOUT_LICENSE")),
+				h(BoxContainer,{},_("ABOUT_DESCRIPTION"))
 			])
 		]),$content);
 	});
 };
 
-const view = (core,proc,win) => (state,actions) => h(Box,{},[
+const view = (core,proc,win,_) => (state,actions) => h(Box,{},[
 	h(Menubar,{},[
-		h(MenubarItem,{ onclick: ev => actions.menu(ev) }, "File"),
-		h(MenubarItem,{ onclick: ev => createAboutDialog(core) },"About")
+		h(MenubarItem,{ onclick: ev => actions.menu(ev) },_("MENU_FILE")),
+		h(MenubarItem,{ onclick: ev => createAboutDialog(core,_) },_("MENU_ABOUT"))
 	]),
 	h(Box,{ grow: 1, shrink: 1 },[
 		state.video ? h(Video,{ src: state.video.url, onload: ev => win.resizeFit(ev.target) }) : null
@@ -45,49 +40,51 @@ const view = (core,proc,win) => (state,actions) => h(Box,{},[
 	].filter(i => !!i)),
 ]);
 
-const openFile = async (core,proc,win,a,file) => {
+const openFile = async (core,proc,win,a,file,_) => {
 	const url = await core.make("osjs/vfs").url(file);
 	const ref = Object.assign({}, file, {url});
 	if(file.mime.match(/^video/)) a.setVideo(ref);
-	win.setTitle(`${proc.metadata.title.en_EN} - ${file.filename}`);
+	win.setTitle(_("WIN_TITLE_PLAYING",file.filename));
 	proc.args.file = file;
 };
 
 const register = (core,args,options,metadata) => {
 	const bus = core.make("osjs/event-handler","VideoPlayer");
+	const {translatable} = core.make("osjs/locale");
+	const _ = translatable(require("./locales.js"));
 	const proc = core.make("osjs/application",{args,options,metadata});
 	proc.createWindow({
 		id: "VideoPlayerWindow",
-		title: metadata.title.en_EN,
+		title: _("WIN_TITLE"),
 		icon: proc.resource(metadata.icon || "icon.png"),
 		dimension: { width: 640, height: 480 }
 	}).on("destroy",() => proc.destroy())
-		.on("render",win => win.focus())
-		.on("drop",(ev,data) => {
-			if(data.isFile && data.mime) {
-				const found = metadata.mimes.find(m => (new RegExp(m)).test(data.mime));
-				if(found) bus.emit('readFile', data);
+	.on("render",win => win.focus())
+	.on("drop",(ev,data) => {
+		if(data.isFile && data.mime) {
+			const found = metadata.mimes.find(m => (new RegExp(m)).test(data.mime));
+			if(found) bus.emit("readFile",data);
+		}
+	}).render(($content,win) => {
+		const a = app({
+			video: null
+		},{
+			setVideo: video => state => ({video}),
+			menu: ev => state => {
+				core.make("osjs/contextmenu").show({
+					menu: [
+						{ label: _("FILE_OPEN"), onclick: () => {
+							core.make("osjs/dialog","file",{ type: "open", mime: metadata.mimes },(btn,item) => {
+								if(btn == "ok") bus.emit("readFile",item);
+							});
+						} },
+						{ label: _("FILE_QUIT"), onclick: () => proc.destroy() }
+					],
+					position: ev.target
+				});
 			}
-		}).render(($content, win) => {
-			const a = app({
-				video: null
-			},{
-				setVideo: video => state => ({video}),
-				menu: ev => state => {
-					core.make("osjs/contextmenu").show({
-						menu: [
-							{ label: "Open", onclick: () => {
-								core.make('osjs/dialog',"file",{ type: "open", mime: metadata.mimes },(btn,item) => {
-									if(btn == "ok") bus.emit("readFile",item);
-								});
-							} },
-							{ label: "Quit", onclick: () => proc.destroy() }
-						],
-						position: ev.target
-					});
-				}
-			},view(core,proc,win),$content);
-		bus.on("readFile",file => openFile(core,proc,win,a,file));
+		},view(core,proc,win,_),$content);
+		bus.on("readFile",file => openFile(core,proc,win,a,file,_));
 		if(args.file) bus.emit("readFile",args.file);
 	});
 	return proc;
---
+++ locales.js
@@ -0,0 +1,20 @@
+const en_US = {
+	WIN_TITLE: "Video Player",
+	WIN_TITLE_PLAYING: "Video Player - {0}",
+	
+	MENU_FILE: "File",
+	
+	FILE_OPEN: "Open",
+	FILE_QUIT: "Quit",
+	
+	MENU_ABOUT: "About",
+	
+	ABOUT_TITLE: "About",
+	
+	ABOUT_VERSION: "Video Player - Version 1.0.0",
+	ABOUT_DEV: "Developed By Spaceboy Ross (https://youtube.com/c/SpaceboyRoss)",
+	ABOUT_LICENSE: "Licenced under Apache 2.0",
+	ABOUT_DESCRIPTION: "A simple video player for OS.js V3 based on the Preview application for OS.js V3."
+};
+
+module.exports = {en_US};
